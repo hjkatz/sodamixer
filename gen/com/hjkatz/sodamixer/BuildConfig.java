5a9f135acc1ba7dc8d0bfa49aa1456bffc4ea290
Harrison Katz <hjkatz03@gmail.com>
2013-01-09 18:55:17 -0500

    intial hello world

--- gen/com/hjkatz/sodamixer/BuildConfig.java
+++ gen/com/hjkatz/sodamixer/BuildConfig.java
@@ -1,8 +1,6 @@
 /** Automatically generated file. DO NOT MODIFY */
-
 package com.hjkatz.sodamixer;
 
-public final class BuildConfig
-{
-	public final static boolean DEBUG = true;
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
 }
---
+++ src/com/hjkatz/sodamixer/activity/LaunchActivity.java
@@ -0,0 +1,27 @@
+package com.hjkatz.sodamixer.activity;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import com.hjkatz.sodamixer.*;
+
+/**
+ * Created with IntelliJ IDEA. User: Harrison Katz Date: 1/9/13 Time: 6:25 PM To change this template use File |
+ * Settings | File Templates.
+ */
+public class LaunchActivity extends Activity
+{
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.launch_activity);
+	}
+
+	public void createButtonClicked(View v)
+	{
+	}
+
+	public void viewButtonClicked(View v)
+	{
+	}
+}
