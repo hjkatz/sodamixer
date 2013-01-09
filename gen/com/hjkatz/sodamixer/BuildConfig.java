dd79552c97ed27de99153d60d6f39570d2c8a1c7
Harrison Katz <hjkatz03@gmail.com>
2013-01-09 18:22:14 -0500

    initial setup

---
+++ gen/com/hjkatz/sodamixer/BuildConfig.java
@@ -0,0 +1,8 @@
+/** Automatically generated file. DO NOT MODIFY */
+
+package com.hjkatz.sodamixer;
+
+public final class BuildConfig
+{
+	public final static boolean DEBUG = true;
+}
---
+++ gen/com/hjkatz/sodamixer/R.java
@@ -0,0 +1,6 @@
+package com.hjkatz.sodamixer;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R
+{
+}
